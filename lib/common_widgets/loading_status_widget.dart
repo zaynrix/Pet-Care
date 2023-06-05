732774469ee9f1735ef198f2d11d0dac4e86edaa
Abdalrhman Abuwarda <114994888+Abdalrhman-Abuwarda@users.noreply.github.com>
2023-06-05 20:41:39 +0300

    add  loading status widget

--- lib/common_widgets/loading_status_widget.dart
+++ lib/common_widgets/loading_status_widget.dart
@@ -1,1 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:pet_care/utils/enums.dart';
+
+class LoadingStatusWidget extends StatelessWidget {
+  const LoadingStatusWidget({
+    required this.loadingStatus,
+    Key? key,
+  }) : super(key: key);
+
+  final LoadingStatusOption loadingStatus;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: SizedBox(
+        height: 150,
+        width: 150,
+        child:  Lottie.asset(animationAssets(loadingStatus.name) ,
+            height: 150,
+            width: 150),
+      ),
+    );
+  }
+}
+
+String animationAssets(String status){
+  switch (status) {
+    case "loading" :
+      {
+        return "assets/json/loading_status.json";
+      }
+    case "error" :
+      {
+        return "assets/json/error_status.json";
+      }
+    case "success" :
+      {
+        return "assets/json/success_status.json";
+      }
+  }
+  return "";
+}
--- lib/utils/enums.dart
+++ lib/utils/enums.dart
@@ -2,3 +2,5 @@ enum AppThemes { light, dark }
 
 enum PetNeuterOption { neuter, notNeuter , notSelected }
 
+enum LoadingStatusOption { loading, success , error }
+
