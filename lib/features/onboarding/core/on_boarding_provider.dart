6b38baf892abe4a4c11f5948576e07e2b9c53f0d
Abdalrhman Abuwarda <114994888+Abdalrhman-Abuwarda@users.noreply.github.com>
2023-06-10 16:56:40 +0300

    add getx

--- lib/features/onboarding/core/on_boarding_provider.dart
+++ lib/features/onboarding/core/on_boarding_provider.dart
@@ -1,17 +1,18 @@
 import 'package:flutter/material.dart';
-class OnBoardingProvider extends ChangeNotifier{
+
+class OnBoardingProvider extends ChangeNotifier {
   final PageController controller = PageController();
   int activeIndex = 0;
   int currantPage = 0;
 
-  void initStateOnBoarding(){
+  void initStateOnBoarding() {
     controller.addListener(() {
       activeIndex = controller.page!.round();
       notifyListeners();
     });
   }
 
-  onoPageChange(int value){
+  onoPageChange(int value) {
     currantPage = value;
     notifyListeners();
   }
