d3bf74b2b95a0ba8721fb951752bd7e688bd2a43
mac <zayns1914@gmail.com>
2023-07-03 15:08:03 +0200

    relocate constructor

--- lib/routing/route_generator.dart
+++ lib/routing/route_generator.dart
@@ -1,12 +1,11 @@
 part of routing_module;
+
 class RouteGenerator {
+  RouteGenerator._();
 
   static const String forgetPasswordScreen = '/forgetPassword';
   static const String resetPasswordScreen = '/resetPassword';
   static const String verifyEmailScreen = '/verifyEmail';
   static const String mainAuthScreen = '/mainAuthScreen';
   static const String mainAddPetScreen = '/mainAddPetScreen';
-
-  RouteGenerator._();
-
 }
