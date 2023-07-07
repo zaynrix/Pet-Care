c4de2ffd81836dd2b8b5c4e97d652fa0bfa49e75
mac <zayns1914@gmail.com>
2023-07-07 19:09:20 +0200

    fix Appbar to Sliver 2

--- lib/features/shop/ui/screens/product_details.dart
+++ lib/features/shop/ui/screens/product_details.dart
@@ -14,6 +14,7 @@ class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: ColorManager.soft,
+      // New Sliver Appbar
       body: CustomScrollView(
         slivers: [
           SliverAppBar(
