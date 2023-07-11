b678d42290738cc120965bbe4bd6e023cc214e6d
Abdalrhman Abuwarda <114994888+Abdalrhman-Abuwarda@users.noreply.github.com>
2023-07-11 13:05:40 +0300

    puck up commit

--- lib/main.dart
+++ lib/main.dart
@@ -51,7 +51,7 @@ class MyApp extends StatelessWidget {
           title: 'Flutter Demo',
           theme: ThemeManager.lightTheme,
           scaffoldMessengerKey: Helpers.scaffoldKey,
-          home: AddReminderScreen(),
+          home: MainShopScreen(),
           navigatorKey: RouteService.serviceNavi.navKey,
           onGenerateRoute: RoutsGenerate.generateRoute,
         ),
