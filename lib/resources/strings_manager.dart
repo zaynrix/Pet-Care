f016bf33f45e4c95b24476207615f3d2983064ed
mac <zayns1914@gmail.com>
2023-07-12 13:39:24 +0200

    add strings

--- lib/resources/strings_manager.dart
+++ lib/resources/strings_manager.dart
@@ -1,5 +1,5 @@
-
-class AppStrings{
+class AppStrings {
+  AppStrings._();
   static const signIn = "Sign In";
   static const signUp = 'Sign Up';
   static const logOut = 'Log out  ';
@@ -10,5 +10,7 @@ class AppStrings{
   static const paymentMethod = 'Payment methods';
   static const addresses = 'Addresses';
   static const apointments = 'Apointments';
+  static const orderPlacedSuccessfully = 'Order placed successfully';
+  static const orderPlacedSuccessfullySub =
+      'You placed the order successfully. Thanks for using our services.';
 }
-
