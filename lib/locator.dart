3929d2348591260e0c1a70ef04df48725a86d112
mac <zayns1914@gmail.com>
2023-07-14 18:24:57 +0200

    add new locator

--- lib/locator.dart
+++ lib/locator.dart
@@ -5,6 +5,8 @@ import 'package:pet_care/data/remote-data/base_client.dart';
 import 'package:pet_care/data/remote-data/end_point.dart';
 import 'package:pet_care/data/remote-data/interceptors/logger_interceptor.dart';
 import 'package:pet_care/features/auth/auth_core/auth_provider.dart';
+import 'package:pet_care/features/home/controllers/home_provider.dart';
+import 'package:pet_care/features/home/repositories/home_repo.dart';
 import 'package:pet_care/features/shop/controllers/product_cotroller.dart';
 import 'package:pet_care/features/shop/repositories/product_repository.dart';
 import 'package:pet_care/resources/size_config.dart';
@@ -20,24 +22,14 @@ void init() {
     ..options.receiveTimeout = const Duration(milliseconds: 30000)
     ..options.responseType = ResponseType.json
     ..options.contentType = Headers.formUrlEncodedContentType
-    // ..options.validateStatus = (statusCode) => statusCode! <= 500
     ..interceptors
         .addAll([DioInterceptor(), if (kDebugMode) LoggerInterceptor()]);
-
-  // Dio client = Dio(
-  //   BaseOptions(
-  //     receiveDataWhenStatusError: true,
-  //     connectTimeout: const Duration(milliseconds: 50000),
-  //     receiveTimeout: const Duration(milliseconds: 50000),
-  //     responseType: ResponseType.json,
-  //     baseUrl: Endpoints.baseUrl,
-  //     contentType: 'application/json',
-  //   ),
-  // );
 
   sl.registerLazySingleton(() => DioClient(client: client));
   sl.registerLazySingleton(() => AuthProvider());
   sl.registerLazySingleton(() => SizeConfig());
   sl.registerLazySingleton(() => ProductController());
   sl.registerLazySingleton(() => ProductRepository());
+  sl.registerLazySingleton(() => HomeProvider());
+  sl.registerLazySingleton(() => HomeRepository());
 }
