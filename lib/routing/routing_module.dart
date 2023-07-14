63db4b7089d29c755d00e7581d50042de771075c
mac <zayns1914@gmail.com>
2023-07-14 18:28:25 +0200

    add imports

--- lib/routing/routing_module.dart
+++ lib/routing/routing_module.dart
@@ -3,10 +3,14 @@ library routing_module;
 import 'package:flutter/material.dart';
 import 'package:pet_care/features/add_pet/add_pet_ui/add_pet_screens/add_pet_screens.dart';
 import 'package:pet_care/features/auth/auth_ui/screens/auth_module.dart';
+import 'package:pet_care/features/home/controllers/home_provider.dart';
+import 'package:pet_care/features/home/ui/home_ui_module.dart';
 import 'package:pet_care/features/location/location_module.dart';
 import 'package:pet_care/features/profile/profile_module.dart';
 import 'package:pet_care/features/reminder/reminder_module.dart';
 import 'package:pet_care/features/shop/ui/screens/shop_screens.dart';
+import 'package:pet_care/locator.dart';
+import 'package:provider/provider.dart';
 
 part 'route_generator.dart';
 part 'route_service.dart';
