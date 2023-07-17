53d8ef275f39698f93da4705fb7643c5100a85af
Abdalrhman Abuwarda <114994888+Abdalrhman-Abuwarda@users.noreply.github.com>
2023-07-17 16:33:41 +0300

    add integration test to ci/cd

---
+++ test/integration_test/shop_integration_test.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+void main(){
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+}
