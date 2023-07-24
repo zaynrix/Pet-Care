54fa7544bf897957d5541cd14f7cd3ae125cb201
mac <zayns1914@gmail.com>
2023-07-24 15:46:04 +0200

    add logic address

--- lib/features/location/ui/screens/add_address_manually.dart
+++ lib/features/location/ui/screens/add_address_manually.dart
@@ -1,57 +1,101 @@
 part of location_module;
 
-class AddNewAddressManually extends StatelessWidget {
-   AddNewAddressManually({Key? key}) : super(key: key);
+class AddNewAddressManually extends StatefulWidget {
+  const AddNewAddressManually({Key? key}) : super(key: key);
+
+  @override
+  State<AddNewAddressManually> createState() => _AddNewAddressManuallyState();
+}
+
+class _AddNewAddressManuallyState extends State<AddNewAddressManually> {
   final TextEditingController streetController = TextEditingController();
   final TextEditingController postCodeController = TextEditingController();
   final TextEditingController suiteController = TextEditingController();
 
   @override
+  void initState() {
+    sl<AddressProvider>().getAddressProvider();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: ElevatedButton(onPressed: null, child: Text("Save"), ),
+      floatingActionButton: const ElevatedButton(
+        onPressed: null,
+        child: Text("Save"),
+      ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       appBar: AppBar(
         title: const Text("Add new Address"),
       ),
-      body: Padding(
-        padding: AppPadding.p24.paddingHorizontal,
-        child: Column(
-          children: [
-            AppSize.s20.addVerticalSpace,
-            InkWell(
-              onTap: () => showModalBottomSheet(context: context,
-                shape: const RoundedRectangleBorder(
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(16),
-                      topRight: Radius.circular(16)),
+      body: Consumer<AddressProvider>(
+        builder: (context, instance, child) => Padding(
+          padding: AppPadding.p24.paddingHorizontal,
+          child: Column(
+            children: [
+              AppSize.s20.addVerticalSpace,
+              InkWell(
+                onTap: () => showModalBottomSheet(
+                  context: context,
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(16),
+                        topRight: Radius.circular(16)),
+                  ),
+                  builder: (BuildContext context) =>
+                      selectLocationType(instance),
                 ),
-                builder: (BuildContext context) => const SelectLocationType(),
+                child: Container(
+                  padding: AppPadding.p24.paddingHorizontal,
+                  height: AppSize.s56.height,
+                  decoration: BoxDecoration(
+                      color: ColorManager.white,
+                      borderRadius: AppSize.s16.circularRadius,
+                      boxShadow: [customShadow()]),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        instance.singleAddress == null
+                            ? "Location Type"
+                            : instance.singleAddress!.type!,
+                        style: bodyRegular(
+                            color: instance.singleAddress == null
+                                ? ColorManager.secondGray
+                                : ColorManager.primary),
+                      ),
+                      const Icon(
+                        Icons.arrow_downward,
+                        color: ColorManager.gray,
+                      )
+                    ],
+                  ),
+                ),
               ),
-              child: Container(
-                padding: AppPadding.p24.paddingHorizontal,
-                height: AppSize.s56.height,
-                decoration: BoxDecoration(
-                  color: ColorManager.white,
-                  borderRadius: AppSize.s16.circularRadius,
-                  boxShadow: [customShadow()]
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text("Location Type", style: bodyRegular(color: ColorManager.secondGray),),
-                    Icon(Icons.arrow_downward , color: ColorManager.gray,)
-                  ],
-                ),
-              ),
-            ),
-            AppSize.s20.addVerticalSpace,
-            CustomTextFormField(hintText: "Street Address", controller: streetController, validator: (value){}),
-            AppSize.s20.addVerticalSpace,
-            CustomTextFormField(hintText: "Apt, Suite, Bldg #", controller: suiteController, validator: (value){}),
-            AppSize.s20.addVerticalSpace,
-            CustomTextFormField(hintText: "Postcode", controller: postCodeController, validator: (value){}),
-          ],
+              AppSize.s20.addVerticalSpace,
+              CustomTextFormField(
+                  hintText: "Street Address",
+                  controller: streetController,
+                  validator: (value) {
+                    return null;
+                  }),
+              AppSize.s20.addVerticalSpace,
+              CustomTextFormField(
+                  hintText: "Apt, Suite, Bldg #",
+                  controller: suiteController,
+                  validator: (value) {
+                    return null;
+                  }),
+              AppSize.s20.addVerticalSpace,
+              CustomTextFormField(
+                  hintText: "Postcode",
+                  controller: postCodeController,
+                  validator: (value) {
+                    return null;
+                  }),
+            ],
+          ),
         ),
       ),
     );
