51aceb02208e9b898b4f636bfd7671dc53cd9cd1
mac <zayns1914@gmail.com>
2023-07-24 15:50:15 +0200

    change stateful to widget

--- lib/features/location/ui/widgets/select_location_type_widget.dart
+++ lib/features/location/ui/widgets/select_location_type_widget.dart
@@ -1,61 +1,76 @@
 part of location_module;
 
-class SelectLocationType extends StatelessWidget {
-  const SelectLocationType({Key? key}) : super(key: key);
+Widget selectLocationType(AddressProvider instance) {
+  return Container(
+    padding: AppSize.s24.paddingAll,
+    height: Get.height * 0.4,
+    decoration: BoxDecoration(
+        color: ColorManager.white, borderRadius: AppSize.s16.circularRadius),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          children: [
+            const Text(
+              "Address Type",
+              style: bodyBoldPrimary,
+            ),
+            const Spacer(),
+            IconButton(
+                onPressed: () {
+                  RouteService.serviceNavi.pop();
+                },
+                icon: const Icon(Icons.cancel, color: ColorManager.primary))
+          ],
+        ),
+        Divider(
+          color: ColorManager.primaryWithTransparent10,
+        ),
+        // AppSize.s20.addVerticalSpace,
+        instance.isLoading == true
+            ? const CustomCircularProgressIndicator()
+            : SizedBox(
+                height: 100,
+                child: ListView.builder(
+                  itemCount: instance.addressModel!.address!.length,
+                  itemBuilder: (context, index) {
+                    return Expanded(
+                      child: InkWell(
+                        onTap: () {
+                          instance.selectAddress(
+                              address: instance.addressModel!.address![index]);
+                          RouteService.serviceNavi.pop();
+                        },
+                        child: Text(
+                          "${instance.addressModel!.address![index].type}",
+                          style: bodyMedium,
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
 
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: AppSize.s24.paddingAll,
-      height: Get.height * 0.4,
-      decoration: BoxDecoration(
-        color: ColorManager.white,
-        borderRadius: AppSize.s16.circularRadius
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
-            children: [
-              const Text("Address Type", style: bodyBoldPrimary,),
-              const Spacer(),
-              IconButton(onPressed: (){
-                RouteService.serviceNavi.pop();
-              }, icon: const Icon(Icons.cancel ,color: ColorManager.primary))
-            ],
-          ),
-          Divider(color: ColorManager.primaryWithTransparent10,),
-          AppSize.s20.addVerticalSpace,
-          Expanded(
-            child: InkWell(
-              onTap: (){
-                RouteService.serviceNavi.pop();
-              },
-              child: const Text("Home" , style: bodyMedium,
-              ),
-            ),
-          ),
-          Expanded(
-            child: InkWell(
-              onTap: (){
-                RouteService.serviceNavi.pop();
-              },
-              child: const Text("Home" , style: bodyMedium,
-              ),
-            ),
-          ),
-          Expanded(
-            child: InkWell(
-              onTap: (){
-                RouteService.serviceNavi.pop();
-              },
-              child: const Text("Other" , style: bodyMedium,
-              ),
-            ),
-          ),
-          AppSize.s20.addVerticalSpace,
-        ],
-      ),
-    );
-  }
+        // Expanded(
+        //   child: InkWell(
+        //     onTap: (){
+        //       RouteService.serviceNavi.pop();
+        //     },
+        //     child: const Text("Home" , style: bodyMedium,
+        //     ),
+        //   ),
+        // ),
+        // Expanded(
+        //   child: InkWell(
+        //     onTap: (){
+        //       RouteService.serviceNavi.pop();
+        //     },
+        //     child: const Text("Other" , style: bodyMedium,
+        //     ),
+        //   ),
+        // ),
+        // AppSize.s20.addVerticalSpace,
+      ],
+    ),
+  );
 }
