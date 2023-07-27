162476a68ed3cdbfbd20ab11b0e2f68ea89e2275
mac <zayns1914@gmail.com>
2023-07-27 14:22:47 +0200

    fix ui card

--- lib/features/home/widgets/vet_card.dart
+++ lib/features/home/widgets/vet_card.dart
@@ -19,14 +19,6 @@ class VetCard extends StatelessWidget {
         height: 180.height,
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(15.0),
-          gradient: LinearGradient(
-            begin: Alignment.bottomCenter,
-            end: Alignment.topCenter,
-            colors: [
-              Colors.black.withOpacity(0.6),
-              Colors.black.withOpacity(0.0),
-            ],
-          ),
         ),
         child: Stack(
           children: [
@@ -81,60 +73,45 @@ class VetCardGrid extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: 12.paddingHorizontal,
-      child: Container(
-        // width: 150.width,
-        height: 600.h,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(15.0),
-          gradient: LinearGradient(
-            begin: Alignment.bottomCenter,
-            end: Alignment.topCenter,
-            colors: [
-              Colors.black.withOpacity(0.6),
-              Colors.black.withOpacity(0.0),
-            ],
-          ),
-        ),
-        child: Stack(
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15.0),
-              ),
-              child: Image.network(
-                vet!.image!,
-                fit: BoxFit.cover,
+      child: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15.0),
+            ),
+            child: Image.network(
+              vet!.image!,
+              fit: BoxFit.cover,
+              // width: 150.width,
+              height: 600.h,
+              errorBuilder: (context, error, stackTrace) => SizedBox(
                 // width: 150.width,
                 height: 600.h,
-                errorBuilder: (context, error, stackTrace) => SizedBox(
-                  // width: 150.width,
-                  height: 600.h,
-                  child: const Center(
-                    child: Icon(Icons.new_releases),
-                  ),
+                child: const Center(
+                  child: Icon(Icons.new_releases),
                 ),
               ),
             ),
-            Positioned(
-              left: 14,
-              bottom: 11,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    vet!.name!,
-                    style: footNoteBold.copyWith(color: ColorManager.primary),
-                  ),
-                  4.addVerticalSpace,
-                  RatingStars(
-                    rating: vet!.review!.toDouble(),
-                  ),
-                ],
-              ),
+          ),
+          Positioned(
+            left: 14,
+            bottom: 11,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  vet!.name!,
+                  style: footNoteBold.copyWith(color: ColorManager.primary),
+                ),
+                4.addVerticalSpace,
+                RatingStars(
+                  rating: vet!.review!.toDouble(),
+                ),
+              ],
             ),
-            const PositionsCardShadow()
-          ],
-        ),
+          ),
+          const PositionsCardShadow()
+        ],
       ),
     );
   }
@@ -147,11 +124,12 @@ class PositionsCardShadow extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Positioned(
-      bottom: 0,
+    return Align(
+      alignment: Alignment.bottomCenter,
       child: Container(
+        height: 100.height,
         decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(15.0),
+          borderRadius: BorderRadius.circular(10.0),
           gradient: LinearGradient(
             begin: Alignment.bottomCenter,
             end: Alignment.topCenter,
@@ -162,129 +140,7 @@ class PositionsCardShadow extends StatelessWidget {
           ),
         ),
         // width: 200.width,
-        height: 100.height,
       ),
     );
   }
 }
-
-// class ShodCardGrid extends StatelessWidget {
-//   ShodCardGrid({
-//     this.singleProduct,
-//     this.onTap,
-//     Key? key,
-//   }) : super(key: key);
-//   final SingleProduct? singleProduct;
-//   final SizeConfig sizeConfig = sl<SizeConfig>();
-//   final Function()? onTap;
-//   @override
-//   Widget build(BuildContext context) {
-//     return Padding(
-//       padding: 12.paddingHorizontal,
-//       child: Container(
-//         height: 600.h,
-//         decoration: BoxDecoration(
-//           boxShadow: [customShadow()],
-//         ),
-//         child: Card(
-//           elevation: 0,
-//           color: ColorManager.white,
-//           shape: RoundedRectangleBorder(
-//             borderRadius: BorderRadius.circular(15.0),
-//           ),
-//           child: Column(
-//             crossAxisAlignment: CrossAxisAlignment.start,
-//             children: [
-//               Stack(
-//                 children: [
-//                   Padding(
-//                     padding: const EdgeInsets.only(
-//                         left: 24, right: 24, top: 14, bottom: 10),
-//                     child: Hero(
-//                       tag: singleProduct!.id!,
-//                       child: Image.network(
-//                         singleProduct!.coverImage!,
-//                         fit: BoxFit.fitWidth,
-//                         height: 120,
-//                         width: 120.w,
-//                         errorBuilder: (context, error, stackTrace) => SizedBox(
-//                           height: 120,
-//                           width: 120.w,
-//                           child: const Center(
-//                             child: Icon(Icons.new_releases),
-//                           ),
-//                         ),
-//                       ),
-//                     ),
-//                   ),
-//                   Positioned(
-//                     bottom: 10,
-//                     left: 10.5,
-//                     child: Container(
-//                       padding: const EdgeInsets.symmetric(
-//                         horizontal: 10.0,
-//                         vertical: 4.0,
-//                       ),
-//                       decoration: BoxDecoration(
-//                         color: const Color(0xFFDFD4FB),
-//                         borderRadius: BorderRadius.circular(50.0),
-//                       ),
-//                       child: Text(
-//                         "%${singleProduct!.discountPercentage} OFF ",
-//                         style: const TextStyle(
-//                           color: ColorManager.primary,
-//                           fontSize: 10.0,
-//                           fontWeight: FontWeight.bold,
-//                         ),
-//                       ),
-//                     ),
-//                   ),
-//                 ],
-//               ),
-//               // const RVerticalSpace(height: AppSize.s10),
-//               Padding(
-//                 padding: 10.paddingHorizontal,
-//                 child: Text(
-//                   "${singleProduct!.name}",
-//                   style: footNoteRegular(color: ColorManager.primary),
-//                   maxLines: 2,
-//                   overflow: TextOverflow.ellipsis,
-//                   textAlign: TextAlign.start,
-//                 ),
-//               ),
-//               Padding(
-//                 padding: 10.paddingHorizontal,
-//                 child: Row(
-//                   children: [
-//                     Text(
-//                       "\$${singleProduct!.price}",
-//                       style: footNoteBold,
-//                     ),
-//                     const RHorizontalSpace(width: AppSize.s5),
-//                     Text(
-//                       "\$${singleProduct!.originalPrice}",
-//                       style: oldPriceStyle,
-//                     ),
-//                     const Spacer(),
-//                     GestureDetector(
-//                         onTap: onTap,
-//                         child: Icon(
-//                           singleProduct!.inCart == true
-//                               ? Icons.add_circle
-//                               : Icons.remove_circle_outline_rounded,
-//                           color: ColorManager.secondary,
-//                         )),
-//                   ],
-//                 ),
-//               ),
-//             ],
-//           ),
-//         ),
-//       )
-//           .animate(delay: 200.ms)
-//           .moveY(begin: -100)
-//           .then()
-//           .shakeY(hz: 4, amount: 3),
-//     );
-//   }
-// }
