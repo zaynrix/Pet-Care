e7030a0602c0eb58f5c1f2d1ff594a9794f53d0d
Abdalrhman Abuwarda <114994888+Abdalrhman-Abuwarda@users.noreply.github.com>
2023-07-31 02:16:43 +0300

    refactor splash screen

--- lib/features/auth/auth_ui/screens/splash_screen.dart
+++ lib/features/auth/auth_ui/screens/splash_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:pet_care/common_widgets/custom_circularProgressIndicator.dart';
 import 'package:pet_care/resources/assets_manager.dart';
-import 'package:pet_care/utils/extension/responsive_extension.dart';
+import 'package:pet_care/resources/colors_manager.dart';
+
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -11,18 +11,62 @@ class SplashScreen extends StatefulWidget {
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _scaleAnimation;
+  late Animation<double> _rotationAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2), // Adjust the duration as needed
+    )..addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        // Animation has completed, you can navigate to the next screen here
+      }
+    });
+
+    _scaleAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(parent: _animationController, curve: Curves.easeInOut),
+    );
+
+    _rotationAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(parent: _animationController, curve: Curves.easeInOut),
+    );
+
+    _animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: ColorManager.secondary,
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SvgPicture.asset(SvgAssets.successAddPet),
-            50.addVerticalSpace,
-            const CustomCircularProgressIndicator()
-          ],
+        child: AnimatedBuilder(
+          animation: _animationController,
+          builder: (context, child) {
+            return Transform.scale(
+              scale: _scaleAnimation.value,
+              child: Transform.rotate(
+                angle: _rotationAnimation.value * 2 * 3.1416,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SvgPicture.asset(SvgAssets.firstScreenPet),
+                  ],
+                ),
+              ),
+            );
+          },
         ),
       ),
     );
