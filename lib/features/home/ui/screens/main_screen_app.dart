bd0a8e91b353edeedf2ef67117b1a081872c47d4
mac <zayns1914@gmail.com>
2023-08-10 17:20:44 +0200

    refactor notification function

--- lib/features/home/ui/screens/main_screen_app.dart
+++ lib/features/home/ui/screens/main_screen_app.dart
@@ -16,79 +16,11 @@ class MainScreenApp extends StatefulWidget {
 }
 
 class _MainScreenAppState extends State<MainScreenApp> {
-  var dataaa = sl<HomeProvider>();
-
   @override
   void initState() {
     super.initState();
-    AwesomeNotifications().isNotificationAllowed().then((isAllowed) {
-      if (!isAllowed) {
-        showDialog(
-          context: RouteService.serviceNavi.navKey.currentContext!,
-          builder: (context) => AlertDialog(
-            title: const Text('Allow Notifications'),
-            content: const Text('Our app would like to send you notifications'),
-            actions: [
-              TextButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                child: const Text(
-                  'Don\'t Allow',
-                  style: TextStyle(
-                    color: Colors.grey,
-                    fontSize: 18,
-                  ),
-                ),
-              ),
-              TextButton(
-                  onPressed: () => AwesomeNotifications()
-                      .requestPermissionToSendNotifications()
-                      .then((_) => Navigator.pop(context)),
-                  child: const Text(
-                    'Allow',
-                    style: TextStyle(
-                      color: Colors.teal,
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ))
-            ],
-          ),
-        );
-      }
-    });
-// pr
-    AwesomeNotifications().createdStream.listen((notification) {
-      ScaffoldMessenger.of(RouteService.serviceNavi.navKey.currentContext!)
-          .showSnackBar(SnackBar(
-        content: Text(
-          'Notification Created on ${notification.channelKey}',
-        ),
-      ));
-    });
-
-    AwesomeNotifications().actionStream.listen((notification) {
-      if (notification.channelKey == 'basic_channel' && Platform.isIOS) {
-        AwesomeNotifications().getGlobalBadgeCounter().then(
-              (value) =>
-                  AwesomeNotifications().setGlobalBadgeCounter(value - 1),
-            );
-      }
-
-      // Navigator.pushAndRemoveUntil(
-      //   context,
-      //   MaterialPageRoute(
-      //     builder: (_) => const PlantStatsPage(),
-      //   ),
-      //   (route) => route.isFirst,
-      // );
-    });
-    // sl<NotificationProvider>().allowNotificationsWidget();
-    print("initState sl HomeProvider/MainScreenApp ${dataaa.hashCode}");
+    sl<NotificationProvider>().allowNotificationsWidget();
     sl<HomeProvider>().getPetsProvider();
-    sl.resetLazySingleton(instance: dataaa);
-    // sl.reset(dispose: true);
   }
 
   @override
@@ -99,7 +31,6 @@ class _MainScreenAppState extends State<MainScreenApp> {
 
   @override
   Widget build(BuildContext context) {
-    print("sl HomeProvider/MainScreenApp/build ${dataaa.hashCode}");
     return Consumer<HomeProvider>(
       builder: (context, controller, child) => Scaffold(
         bottomNavigationBar: BottomNavigationBar(
